da4762758f4140d0ec4a37cec78a637ffa496738
Biebzoom008 <theodelalayx@gmail.com>
2024-10-29 08:12:00 +0100

    Working game
    I finally managed to make the win conditions work!!
    Although, there is a small problem left. If you enter anything else than an Int when choosing the position it will result in an error. I have not yet learned how to catch errors and will probably not implement it in this "game". It was, after all, only a small exercise (that took me hours hahahah)
    
    If anyone reading has suggestions on how to improve my code, then please, feel free to text me :3

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,18 @@ public class Main {
         //Initialization of the board and scanner, and also a boolean board that is used to check whether someone played on one spot or not
         String[][] board = new String[3][3];
         boolean[][] boardCheck = new boolean[3][3];
+        int[][] boardWin = new int [3][3];
+
+        int winner = 0; // initializes a variable used at the end when displaying who the winner is (1 = player 1, 2 = player 2)
 
         //This loop puts the boolean value false everywhere in the array so that when a player chooses a spot its value turns to true to avoid playing multiple times on the same spot
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 boardCheck[i][j] = false;
                 board[i][j] = "empty";
+                boardWin[i][j] = 0;
             }
         }
-
 
         Scanner scanner = new Scanner(System.in);
         boolean boardNotFull = board[0][0].equals("empty") || board[0][1].equals("empty") || board[0][2].equals("empty") || board[1][0].equals("empty") || board[1][1].equals("empty") || board[1][2].equals("empty") || board[2][0].equals("empty") || board[2][1].equals("empty") || board[2][2].equals("empty");
@@ -36,9 +39,8 @@ public class Main {
         boardPrintCheck(boardCheck); //Shows the initialization of the "memory" of the spots already taken in the game
 
 
-
-        //game
-        while (boardNotFull) {
+        //Game
+        while (boardNotFull && !win(boardWin)) { //If the board is full, or if someone won, then the loop will stop
 
             //Player 1
             System.out.println("Player 1's turn");
@@ -47,100 +49,34 @@ public class Main {
             System.out.println("Choose a column: ");
             int column = scanner.nextInt();
 
-            while ((line > 2 || column > 2)|| (boardCheck[line][column] == true)) {
+            while ((line > 2 || column > 2)|| (boardCheck[line][column] == true)) { //Replays the turn if the spot chosen has already been taken or if the chosen position is not valid
                 if (line > 2 || column > 2) {
                     System.out.println("Choose a valid position from these numbers: (0 - 1 - 2)");
-                    System.out.println("Choose a line: ");
-                    line = scanner.nextInt();
-                    System.out.println("Choose a column: ");
-                    column = scanner.nextInt();
                 }
                 if (boardCheck[line][column] == true) {
-                    System.out.println("That position has already been used."); //Replays the turn if the spot chosen has already been taken
-                    System.out.println("Choose a line: ");
-                    line = scanner.nextInt();
-                    System.out.println("Choose a column: ");
-                    column = scanner.nextInt();
+                    System.out.println("That position has already been used.");
                 }
+                System.out.println("Choose a line: ");
+                line = scanner.nextInt();
+                System.out.println("Choose a column: ");
+                column = scanner.nextInt();
             }
             //assigns the player 1 symbol to the position chosen, and its position is set to true
             board[line][column] = player1Symbol;
             boardCheck[line][column] = true;
-
+            boardWin[line][column] = 1;
+            winner = 1;
             boardPrint(board);
+            boardIntPrint(boardWin);
+            System.out.println(win(boardWin));
 
-            //win condition
-            if(board[0][0].equals(board[0][1].equals(board[0][2].equals(player1Symbol))) || board[0][0].equals(board[0][1].equals(board[0][2].equals(player2Symbol)))){
-                if(board[0][0].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[1][0].equals(board[1][1].equals(board[1][2].equals(player1Symbol))) || board[1][0].equals(board[1][1].equals(board[1][2].equals(player2Symbol)))){
-                if(board[1][0].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[2][0].equals(board[2][1].equals(board[2][2].equals(player1Symbol))) || board[2][0].equals(board[2][1].equals(board[2][2].equals(player2Symbol)))){
-                if(board[2][0].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[0][0].equals(board[1][0].equals(board[2][0].equals(player1Symbol))) || board[0][0].equals(board[1][0].equals(board[2][0].equals(player2Symbol)))){
-                if(board[0][0].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[0][1].equals(board[1][1].equals(board[2][1].equals(player1Symbol))) || board[0][0].equals(board[1][1].equals(board[2][1].equals(player2Symbol)))){
-                if(board[0][1].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[0][2].equals(board[1][2].equals(board[2][2].equals(player1Symbol))) || board[0][2].equals(board[1][2].equals(board[2][2].equals(player2Symbol)))){
-                if(board[0][2].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[0][0].equals(board[1][1].equals(board[2][2].equals(player1Symbol))) || board[0][0].equals(board[1][1].equals(board[2][2].equals(player2Symbol)))){
-                if(board[0][0].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
-            else if(board[0][2].equals(board[1][1].equals(board[2][0].equals(player1Symbol))) || board[0][2].equals(board[1][1].equals(board[2][0].equals(player2Symbol)))){
-                if(board[2][0].equals(player1Symbol)) {
-                    System.out.println("Congrats, player 1 wins!!");
-                    break;
-                } else {System.out.println("Congrats, player 2 wins!!");
-                    break;
-                }
-            }
 
 
 
 
             //Player 2 (The "Big" if is used to check if the board is full or not)
             //if yes, then player 2 won't play again and again
-            if(boardNotFull) {
+            if(boardNotFull && !win(boardWin)) {
                 System.out.println("Player 2's turn");
                 System.out.println("Choose a line: ");
                 line = scanner.nextInt();
@@ -150,29 +86,35 @@ public class Main {
                 while ((line > 2 || column > 2)|| (boardCheck[line][column] == true)){ //The while and if are used to check for "errors", mainly to avoid a ArrayOutOfBounds exception, or to avoid playing on a used spot
                     if (line > 2 || column > 2){
                         System.out.println("Choose a valid position from these numbers: (0 - 1 - 2)");
-                        System.out.println("Choose a line: ");
-                        line = scanner.nextInt();
-                        System.out.println("Choose a column: ");
-                        column = scanner.nextInt();
                     }
                     if (boardCheck[line][column] == true) {
                         System.out.println("That position has already been used."); //Replays the turn if the spot chosen has already been taken
-                        System.out.println("Choose a line: ");
-                        line = scanner.nextInt();
-                        System.out.println("Choose a column: ");
-                        column = scanner.nextInt();
                     }
+                    System.out.println("Choose a line: ");
+                    line = scanner.nextInt();
+                    System.out.println("Choose a column: ");
+                    column = scanner.nextInt();
                 }
 
                 board[line][column] = player2Symbol;
                 boardCheck[line][column] = true;
-
+                boardWin[line][column] = 7;
+                winner = 2;
                 boardPrint(board);
+                boardIntPrint(boardWin);
+
+                System.out.println();
             }
 
         }
 
-        boardPrint(board);
+        if (winner == 1){
+            System.out.println("Congratulations, player 1 wins!!");
+        } else {
+            System.out.println("Congratulations, player 2 wins!!");
+        }
+
+
     }
 
 
@@ -212,54 +154,41 @@ public class Main {
         }
     }
 
-    public static void win (String[][] board, String player1Symbol){
-        if(board[0][0].equals(board[0][1].equals(board[0][2]))){
-            if(board[0][0].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
+    public static void boardIntPrint (int[][] board){
+        int counter = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+                counter += 1;
+                if (counter == 3) {
+                    System.out.println("\n");
+                    counter = 0;
+                }
+
             }
-        }
-        else if(board[1][0].equals(board[1][1].equals(board[1][2]))){
-            if(board[1][0].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
-        }
-        else if(board[2][0].equals(board[2][1].equals(board[2][2]))){
-            if(board[2][0].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
-        }
-        else if(board[0][0].equals(board[1][0].equals(board[2][0]))){
-            if(board[0][0].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
-        }
-        else if(board[0][1].equals(board[1][1].equals(board[2][1]))){
-            if(board[0][1].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
-        }
-        else if(board[0][2].equals(board[1][2].equals(board[2][2]))){
-            if(board[0][2].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
-        }
-        else if(board[0][0].equals(board[1][1].equals(board[2][2]))){
-            if(board[0][0].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
-        }
-        else if(board[0][2].equals(board[1][1].equals(board[2][0]))){
-            if(board[2][0].equals(player1Symbol)) {
-                System.out.println("Congrats, player 1 wins!!");
-            } else {System.out.println("Congrats, player 2 wins!!");
-            }
+
         }
     }
+
+    //This method checks all possible win conditions each player loop and returns true if there is at least one
+    public static boolean win (int[][] board){
+                int win1 =(board [0][0] + board[0][1] + board[0][2]);
+                int win2 = (board[1][0] + board[1][1] + board[1][2]);
+                int win3 = (board[2][0] + board[2][1] + board[2][2]);
+                int win4 = board[0][0] + board[1][0] + board[2][0];
+                int win5 = board[0][1] + board[1][1] + board[2][1];
+                int win6 = board[0][2] + board[1][2] + board[2][2];
+                int win7 = board[0][0] + board[1][1] + board[2][2];
+                int win8 = board[0][2] + board[1][1] + board[2][0];
+
+                if(win1 == 3 || win2 == 3 || win3 == 3 || win4 == 3 || win5 == 3 || win6 == 3 || win7 == 3 || win8 == 3){
+                    return true;
+                } else if (win1 == 21 || win2 == 21 || win3 == 21 || win4 == 21 || win5 == 21 || win6 == 21 || win7 == 21 || win8 == 21){
+                    return true;
+                }
+
+
+        return false;
+    }
+
 }
